7606e434949d8dc17037b3ef7f433e10dfb72f07
Filipe <filipe.rj149@gmail.com>
2020-07-13 15:31:11 -0300

    quotation classes fixed

--- Conversor de valores monetários.py
+++ Conversor de valores monetários.py
@@ -2,18 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 
 r = requests.get('https://www.valor.com.br/valor-data') #requisição do site
+soup = BeautifulSoup(r.text, 'html.parser')
 
 '''Dólar'''
-soupdolar = BeautifulSoup(r.text, 'html.parser')
-cotacaodolar = soupdolar.find(class_='number')
+cotacaodolar = soup.find_all(class_='data-cotacao__ticker_quote')[0]
 valorcotacaodolar = float((cotacaodolar.text).replace(',', '.'))
 
-
 '''Euro'''
-soupeuro = BeautifulSoup(r.text, 'html.parser')
-cotacaoeuro = soupeuro.find(class_='item')
-cotacaoeuro2 = soupeuro.find_all('span')[6]
-valorcotacaoeuro = float((cotacaoeuro2.text).replace(',', '.'))
+cotacaoeuro = soup.find_all(class_='data-cotacao__ticker_quote')[3]
+valorcotacaoeuro = float((cotacaoeuro.text).replace(',', '.'))
 
 msg = 'Conversor de valores monetários'
 msg2 = 'Script feito por Filipe Miranda, os valores utilizados são do site: https://www.valor.com.br/valor-data'
